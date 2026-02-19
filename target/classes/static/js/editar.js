2607a6e23897903f55e2c0a04d9e691216ab5587
KusonokiLynch <dt209379@gmail.com>
2026-02-19 15:06:35 -0500

    remove target build files

--- target/classes/static/js/editar.js
+++
@@ -1,9 +0,0 @@
-function FormularioEdit(id) {
-            const row = document.getElementById("formulario-" + id);
-             if (row.style.display === "table-row") {
-                 row.style.display = "none";
-             } 
-             else {
-                row.style.display = "table-row";
-              }
-        }
